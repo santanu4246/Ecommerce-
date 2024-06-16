2d145138e5b792a99bd30f7288157e445dbd61bc
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-06-16 13:26:28 +0530

    navbarResponsive done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,32 +1,49 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import Nav from './Nav'
-import './Header.css'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Nav from './Nav';
+import './Header.css';
 import { MdOutlineShoppingCart } from "react-icons/md";
+import { WiNightFog } from 'react-icons/wi';
+
 const Header = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
     return (
         <nav>
             <div className="navbar">
                 <Link to="/"><div className="logo">SantanuKart</div></Link>
-                <div className="nav-lists">
-                    <Link to="/"><li>HOME</li></Link>
-                    <Link to="/about"><li>ABOUT</li></Link>
-                    <Link to="/profucts"><li>PRODUCTS</li></Link>
-                    <Link to="/contact"><li>CONTACT</li></Link>
-                    <button>LOGIN IN</button>
-                    <Link to="/cart"><div className="cart">
-                        <MdOutlineShoppingCart id='cart-icon' />
-
-                            <span className='items-count'>0</span>
-                        </div>
-                    </Link>
-                </div>
+                {!isMobile && (
+                    <div className="nav-lists">
+                        <Link to="/"><li>HOME</li></Link>
+                        <Link to="/about"><li>ABOUT</li></Link>
+                        <Link to="/products"><li>PRODUCTS</li></Link>
+                        <Link to="/contact"><li>CONTACT</li></Link>
+                        <button>LOGIN IN</button>
+                        <Link to="/cart">
+                            <div className="cart">
+                                <MdOutlineShoppingCart id='cart-icon' />
+                                <span className='items-count'>10</span>
+                            </div>
+                        </Link>
+                    </div>
+                )}
+                {isMobile && <Nav />}
             </div>
         </nav>
-
-    )
+    );
 }
 
-
-export default Header
+export default Header;
--- src/Components/Header/Nav.jsx
+++ src/Components/Header/Nav.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { RiMenu3Fill } from "react-icons/ri";
+import { FaXmark } from "react-icons/fa6";
+import "./Nav.css";
+import { Link } from 'react-router-dom';
+import { MdOutlineShoppingCart } from "react-icons/md";
 
 const Nav = () => {
+  const [mobilenav, setmobilenav] = useState(false);
+
+  const handleCrossClick = () => {
+    setmobilenav(false);
+  };
+
+  const handleMenuClick = () => {
+    setmobilenav(true);
+  };
+
   return (
-   <nav>
-    
-   </nav>
-  )
+    <>
+      <div className="mobile-nav">
+        <RiMenu3Fill className='mobile-nav-icon' onClick={handleMenuClick} />
+      </div>
+      <div className={`mob-nav ${mobilenav ? 'open' : ''}`}>
+        <FaXmark className='mobile-nav-icon' style={{position: "absolute", top: "25px", right: "20px"}} onClick={handleCrossClick} />
+        <Link to="/"><li onClick={handleCrossClick}>HOME</li></Link>
+        <Link to="/about"><li onClick={handleCrossClick}>ABOUT</li></Link>
+        <Link to="/products"><li onClick={handleCrossClick}>PRODUCTS</li></Link>
+        <Link to="/contact"><li onClick={handleCrossClick}>CONTACT</li></Link>
+        <button onClick={handleCrossClick}>LOGIN IN</button>
+        <Link to="/cart">
+          <div className="cart">
+            <MdOutlineShoppingCart id='cart-icon' style={{fontSize: "2.5rem"}} onClick={handleCrossClick}/>
+            <span className='items-count' onClick={handleCrossClick}>0</span>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
 }
 
-export default Nav
+export default Nav;
