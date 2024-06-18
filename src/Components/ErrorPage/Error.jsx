c38d5601ff5cb67e78227b920bf56fb4404c448b
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-06-18 12:56:39 +0530

    Error Page

--- src/Components/ErrorPage/Error.jsx
+++ src/Components/ErrorPage/Error.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import './Error.css'
+import { Link } from 'react-router-dom'
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className='ErrorContainer'>
+      
+      <h1>404</h1>
+      <h2>UH OH! You're lost'</h2>
+      <span>The page you are looking does not exixt. How you get here is a mystery. But you can click the button below to go back to the homepage</span>
+      <Link to="/"><button>GO TO HOME PAGE</button></Link>
+    </div>
   )
 }
 
