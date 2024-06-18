0922d802b1c9a8205b498d60d4e5a43c41105e6b
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-06-18 09:50:39 +0530

    Trusted Section

--- src/Components/Trusted.jsx
+++ src/Components/Trusted.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import './Trusted.css';
 
 const Trusted = () => {
   return (
-    <div>Trusted</div>
-  )
+    <div className='TrustedContainer'>
+      <h3>Trusted By 1000+ Companies</h3>
+      <div className="TrustesCompanyLogos">
+        <div className="slide">
+          <img
+            src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"
+            alt="trusted-brands"
+          />
+        </div>
+        <div className="slide">
+          <img
+            src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png"
+            alt="trusted-brands"
+          />
+        </div>
+        <div className="slide">
+          <img
+            src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image4.png"
+            alt="trusted-brands"
+          />
+        </div>
+        <div className="slide">
+          <img
+            src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image6.png"
+            alt="trusted-brands"
+          />
+        </div>
+        <div className="slide">
+          <img
+            src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"
+            alt="trusted-brands"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Trusted
+export default Trusted;
